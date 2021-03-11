b35dd675ef7b83e0818174a1c386ab19ecbc71d7
Sahabuddin Shakil <mdshakil8796@gmail.com>
2021-03-11 13:40:34 +0600

    explain while loop python

---
+++ while_loop.py
@@ -0,0 +1,28 @@
+# name =""
+# while name != "your name":
+#     print("Enter your name:")
+#     name = input()
+# print("Thank You")    
+# while name =="":
+#     print("yes")
+#     name ="shakil"
+# print("NO")    
+
+# while True:
+#     print("Enter your name:")
+#     name  = input()
+#     if name =="Shakil":
+#         break
+
+# while loop learn
+
+while True:
+    print("Enter Your name:")
+    name = input()
+    if name != "Shakil":
+        continue
+    print("Hey Shakil Enter your passward:")
+    passw = input()
+    if passw =="shakil645914":
+        break
+print("Congratulations")        
