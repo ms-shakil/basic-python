3f68558926cf85943bd4a1f8fb00d3efc6bcaade
Sahabuddin Shakil <mdshakil8796@gmail.com>
2021-03-22 14:04:01 +0600

    about dictonary

--- about_dictionari.py
+++ about_dictionari.py
@@ -18,7 +18,7 @@ for i in mydic.items():
     print(i)
 
 ## string cha count 
-data ="Hi my name is payer ahamed . i am  software and civil engineer. i will got 3 married insaallah"
+data ="Hi my name is payer ahamed . i am a software engineer. Oh i also have Civil engineering degrees. i will got 3 married insaallah"
 dic={}
 
 for ch in data:
@@ -26,3 +26,25 @@ for ch in data:
     dic[ch] = dic[ch]+ 1
 print(dic)
 print(format(dic))
+import pprint
+def printF(dic):
+    print("The computer will go first.")
+    print("{} | {} | {}".format(dic["Top-L"],dic["Top-M"],dic["Top-R"]))
+    print("________")
+    print("{} | {} | {}".format(dic["Mid-L"],dic["Mid-M"],dic["Mid-R"]))
+    print("________")
+    print("{} | {} | {}".format(dic["Bot-L"],dic["Bot-M"],dic["Bot-R"]))
+
+data={"Top-L":" ","Top-M":" ","Top-R":" ",
+    "Mid-L":" ","Mid-M":" ","Mid-R":" ",
+    "Bot-L":" ","Bot-M":" ","Bot-R":" "}
+data["Top-L"]="O"
+data["Top-M"]="O"
+data["Top-R"]="X"
+data["Mid-L"]="O"
+data["Mid-M"]="X"
+data["Mid-R"]="O"
+data["Bot-R"]="X"
+data["Bot-L"]="X"
+data ["Bot-M"]="X"
+printF(data)
